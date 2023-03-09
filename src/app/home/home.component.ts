44f8cc2142ebd01d1ff8fdd1fbd42b63709dfc0f
tan-kun <1712huynhtan@gmail.com>
2023-03-09 14:27:34 +0700

    First commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+
+export class NgModalConfirm {
+  constructor(public modal: NgbActiveModal) { }
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent implements OnInit {
+  employeeList: any[];
+
+  constructor(
+    private router: Router,
+    private modalService: NgbModal,
+    private toastr: ToastrService,
+  ) {
+    this.employeeList = [];
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  redirectAddEmployee = () => {
+    this.router.navigate(['/user']);
+  };
+
+  deleteEmployeeConfirmation(employee: any) {
+    this.modalService.open(NgModalConfirm,
+      {
+        ariaLabelledBy: 'modal-basic-title'
+      }).result.then(() => {
+        this.deleteEmployee(employee);
+      },
+      () => {});
+  }
+
+  private deleteEmployee(employee: any) {
+
+  }
+}
